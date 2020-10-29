82764231008365105a3a663b2dd9bf96c124fdee
Mike <43199789+Politanskyi@users.noreply.github.com>
2020-10-30 01:58:17 +0200

    Update val-filter.js
    Fix val form getter

--- src/js/usefull/val-filter.js
+++ src/js/usefull/val-filter.js
@@ -2,36 +2,22 @@ import { filter } from "js#/usefull/years-filter";
 
 export function getFilterVal () {
 
-    let filterArr = $('.js-filters').find('input, select');
     let pageNum = $('.pagination__number--current').find('span').text();
     const filterData = new FormData(filter[0]);
     let filterVal = {
-        params: {},
-        pagination: {}
-    };
+        params: {
+                brand: filterData.getAll('brand');
+                manufacturer: filterData.get('manufacturer');
+                model: filterData.get('model');
+                year: Number(filterData.get('year'));
+                price: filterData.getAll('price').map((str) => Number(str));
+                },
+        pagination: {
+                sort: filterData.get('sort');
+                perPage: Number(filterData.get('perPage'));
+                page: Number(pageNum);
+                }
+        };
 
-    filterArr.each(function (index, element) {
-        let el = $(element);
-        let elName = el.attr('name');
-
-        if (el.val()) {
-            if (elName === 'brand') {
-                filterVal.params[elName] = filterData.getAll(elName);
-            } else if (elName === 'manufacturer') {
-                filterVal.params[elName] = filterData.get(elName);
-            } else if (elName === 'model') {
-                filterVal.params[elName] = filterData.get(elName);
-            } else if (elName === 'year') {
-                filterVal.params[elName] = Number(filterData.get(elName));
-            } else if (elName === 'price') {
-                filterVal.params[elName] = filterData.getAll(elName).map((str) => Number(str));
-            } else if (elName === 'sort') {
-                filterVal.pagination[elName] = filterData.get(elName);
-            } else if (elName === 'perPage') {
-                filterVal.pagination[elName] = Number(filterData.get(elName));
-            }
-            filterVal.pagination.page = Number(pageNum);
-        }
-    });
     console.log(filterVal);
 }
